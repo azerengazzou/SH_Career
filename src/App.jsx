837288b541a1eebfb664e723ae54a44019e80565
azer.engazzou <azer.engazzou@corilus.be>
2024-01-15 18:06:16 +0100

    Delete home page files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 
-import Home from "./pages/Home";
+import Builder from "./pages/Builder";
 
 function App() {
   return (
     <div>
-      <Home/>
+      <Builder/>
     </div>
   );
 }
-
 export default App;
